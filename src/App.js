1b9ffb03e8456550a2078f18f1a2c8fb576bc6a2
ivanmolina <ivanmolinabarba@gmail.com>
2022-03-21 19:30:56 -0300

    addtoCart

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
 import FunctionalComponent from './components/Functional';
 
-
 let booleanTst = true;
 
 const color = booleanTst ? 'pink' : 'blue';
@@ -16,15 +15,36 @@ const styles = {
   typesContainer: {
   display:'flex',
   flexDirection:'column',
-  
-  
   },
 };
 
 const personaFuncion = {
   nombre:'Ivan',
   apellido:'Molina',
+};
 
+const listProduct = [
+  {
+    nombre: "Charizard",
+    stock:1,
+    initial:2500
+  },
+  {
+    nombre: "Blastoise",
+    stock:2,
+    initial:2000
+  },
+  {
+    nombre: "Venasaur",
+    stock:3,
+    initial:1200
+  },
+];
+
+const stock = 10;
+const initial=1;
+const onAdd = (contador) =>{
+    console.log('click' , contador);
 };
 
 function App() {
@@ -33,26 +53,18 @@ function App() {
       <header className="App-header">
         <NavBar />
         <div style={styles.typesContainer}>
-          <FunctionalComponent
-          nombre={personaFuncion.nombre}
-          apellido={personaFuncion.apellido}
-          children={<img src={logo} className="App-logo" alt="logo" />} 
-        />
+          {listProduct.map((producto) => (
+              <FunctionalComponent
+              stock={producto.stock}
+              initial={producto.initial}
+              onAdd={onAdd}
+              />
+
+              ))}
+
         </div>
-        <p style={styles.title} o>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
 export default App;
--- src/components/Functional.js
+++ src/components/Functional.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const ItemListContainer = ({stock , initial , onAdd}) => {
+    const [contador, setContador] = useState(initial);
 
-const FunctionalComponent = ({nombre,apellido,children}) => {
+    const clickSum = () => {
+        if (contador === stock) {
+            return;
+        }
+            setContador(contador + 1);
+    };
+
+    const clickRes = () => {
+        if (contador === initial) {
+            return;
+        }
+       
+            setContador(contador - 1);
+        };
+
+    const addToCart = () => {
+        onAdd (contador);
+    };
     return (
-       <div>
-           <h3>Venta de Cartas Pokemon</h3>
-           <h2>{nombre}</h2>
-           <h2>{apellido}</h2>
-           {children}
-       </div>
+        <div>
+            <h3>Venta de Cartas Pokemon</h3>
+            <h1>{contador}</h1>
+             
+            <div style={{
+                display: 'flex', 
+                flexDirection: 'row',
+                justifyContent: 'center',
+            }}>
+                <button onClick={() => clickSum()}>Sumar 1</button>
+                <button onClick={() => clickRes()}>Restar 1</button>
+            </div>
+            <div>
+                <button onClick={() => addToCart() }>Add</button>
+            </div>
+        </div>
     );
 };
 
-export default FunctionalComponent;
+export default ItemListContainer;
